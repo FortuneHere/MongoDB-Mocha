521c019a050dfca2e24397efaaf6a688e9a6973e
Kekray <kekray777@gmail.com>
2022-11-25 07:36:07 +0400

    validation tests

--- src/student.js
+++ src/student.js
@@ -3,8 +3,19 @@ const Schema = mongose.Schema;
 
 // Создаем схему для модели
 const StudentSchema = new Schema({
-  name: String,
+  name: {
+    type: String,
+    // Требует чтобы имя было введено, [true, сообщение]
+    required: [true, 'Name is required'],
+    // Условие, сообщение
+    validate: {
+      validator: (name) => name.length > 2,
+      message: 'Name is too short'
+    }
+  },
   studentNumber: Number,
+  articleCount: Number,
+  grade: Number,
 });
 
 // Создаем саму модель
